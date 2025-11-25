05caadc7f49d77eb48e3a187db084dae8cf574f4
Hồ Thanh Phú <hophu234632@gmail.com>
2025-11-25 23:37:28 +0700

    update main.cpp

---
+++ main.cpp
@@ -0,0 +1,12 @@
+#include "QuizSession.h"
+#include <iostream>
+using namespace std;
+int main() {
+
+    ios_base::sync_with_stdio(false);
+    QuizSession session;
+    session.run();
+    return 0;
+}
+
+
